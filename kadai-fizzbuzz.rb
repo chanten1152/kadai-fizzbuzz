ec13cdec96502beea55535aa243ce9fd9f12c96d
takatoshi.tenkou <daiten.business@gmail.com>
2021-07-15 08:05:36 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+
+
+def fizzbuzz(num)
+    if num % 15 == 0
+        puts 'FizzBuzz'
+    elsif num % 5 == 0
+        puts 'Buzz'
+    elsif num % 3 == 0 
+        puts 'Fizz'
+    else
+        return num
+    end
+end
+num_max = 100
+(1..num_max).each do |num|
+  puts fizzbuzz(num)
+end
